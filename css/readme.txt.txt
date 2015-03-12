1785ce989faa6770781dc53cd35bc3259a492d98
Joel Moya <joel.moya@jalasoft.com>
2015-03-12 11:43:41 -0400

    Modifying files

---
+++ css/readme.txt.txt
@@ -0,0 +1,1 @@
+Here classes to styles
--- src/models/player.js
+++ src/models/player.js
@@ -4,7 +4,7 @@
  */
 var Player = function() {
     this.field = new Field();
-    // this.name = window.prompt('Your name?'); it should be unccomment if you can put name
+    // this.name = window.prompt('Your name?'); it should be unccomment if you can put name 
     this.name = 'Joel';
     this.isLooser = function() {
         return this.field.isAnyShipAlive();
